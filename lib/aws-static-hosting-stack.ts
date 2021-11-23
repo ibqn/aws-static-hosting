46c5acd678ae4336f367f9d5517902499380c023
ibqn <evgen.ibqn@gmail.com>
2021-11-24 00:21:48 +0100

    add deployment bucket

--- lib/aws-static-hosting-stack.ts
+++ lib/aws-static-hosting-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from "@aws-cdk/core";
 // import * as sqs from '@aws-cdk/aws-sqs';
 import { Bucket, BucketAccessControl } from "@aws-cdk/aws-s3";
+import { BucketDeployment, Source } from "@aws-cdk/aws-s3-deployment";
+import * as path from "path";
 
 export class AwsStaticHostingStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -15,6 +17,11 @@ export class AwsStaticHostingStack extends cdk.Stack {
 
     const bucket = new Bucket(this, "Bucket", {
       accessControl: BucketAccessControl.PRIVATE,
+    });
+
+    new BucketDeployment(this, "BucketDeployment", {
+      destinationBucket: bucket,
+      sources: [Source.asset(path.resolve(process.cwd(), "dist"))],
     });
   }
 }
