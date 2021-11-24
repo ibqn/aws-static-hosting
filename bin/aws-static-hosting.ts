b02d7a4663a2764bb6d60b75c8fc14536e586f56
ibqn <evgen.ibqn@gmail.com>
2021-11-24 08:52:24 +0100

    add prettier and editorconfig

--- bin/aws-static-hosting.ts
+++ bin/aws-static-hosting.ts
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
-import "source-map-support/register";
-import * as cdk from "@aws-cdk/core";
-import { AwsStaticHostingStack } from "../lib/aws-static-hosting-stack";
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { AwsStaticHostingStack } from '../lib/aws-static-hosting-stack'
 
-const app = new cdk.App();
-new AwsStaticHostingStack(app, "AwsStaticHostingStack", {
+const app = new cdk.App()
+new AwsStaticHostingStack(app, 'AwsStaticHostingStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+})
 
-app.synth();
+app.synth()
--- lib/aws-static-hosting-stack.ts
+++ lib/aws-static-hosting-stack.ts
@@ -1,14 +1,14 @@
-import * as cdk from "@aws-cdk/core";
-// import * as sqs from '@aws-cdk/aws-sqs';
-import { Bucket, BucketAccessControl } from "@aws-cdk/aws-s3";
-import { BucketDeployment, Source } from "@aws-cdk/aws-s3-deployment";
-import { Distribution, OriginAccessIdentity } from "@aws-cdk/aws-cloudfront";
-import { S3Origin } from "@aws-cdk/aws-cloudfront-origins";
-import * as path from "path";
+import * as cdk from '@aws-cdk/core'
+// import * as sqs from '@aws-cdk/aws-sqs'
+import { Bucket, BucketAccessControl } from '@aws-cdk/aws-s3'
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment'
+import { Distribution, OriginAccessIdentity } from '@aws-cdk/aws-cloudfront'
+import { S3Origin } from '@aws-cdk/aws-cloudfront-origins'
+import * as path from 'path'
 
 export class AwsStaticHostingStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // The code that defines your stack goes here
 
@@ -17,26 +17,26 @@ export class AwsStaticHostingStack extends cdk.Stack {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
 
-    const bucket = new Bucket(this, "Bucket", {
+    const bucket = new Bucket(this, 'Bucket', {
       accessControl: BucketAccessControl.PRIVATE,
-    });
+    })
 
-    new BucketDeployment(this, "BucketDeployment", {
+    new BucketDeployment(this, 'BucketDeployment', {
       destinationBucket: bucket,
-      sources: [Source.asset(path.resolve(process.cwd(), "dist"))],
-    });
+      sources: [Source.asset(path.resolve(process.cwd(), 'dist'))],
+    })
 
     const originAccessIdentity = new OriginAccessIdentity(
       this,
-      "OriginAccessIdentity"
-    );
-    bucket.grantRead(originAccessIdentity);
+      'OriginAccessIdentity'
+    )
+    bucket.grantRead(originAccessIdentity)
 
-    new Distribution(this, "Distribution", {
-      defaultRootObject: "index.html",
+    new Distribution(this, 'Distribution', {
+      defaultRootObject: 'index.html',
       defaultBehavior: {
         origin: new S3Origin(bucket, { originAccessIdentity }),
       },
-    });
+    })
   }
 }
